c97a84c9a0a0cb119f7d8ebc6a4c1953d1c376d7
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-07-17 01:49:03 +0100

    Phone Number Analysis

---
+++ phone-number-analysis/PhoneNumberAnalysis.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+
+public static class PhoneNumber
+{
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(
+        string phoneNumber
+    )
+    {
+        string[] splitNumber = phoneNumber.Split('-');
+        return (
+            splitNumber[0] == "212", splitNumber[1] == "555", splitNumber[2]
+        );
+    }
+
+    public static bool IsFake(
+        (bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo
+    ) => phoneNumberInfo.IsFake;
+}
