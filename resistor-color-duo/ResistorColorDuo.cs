744e45672adc2843406c8b08ca30ebb1d7e8232a
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-07-19 22:01:08 +0100

    Resistor Color Duo solved

--- resistor-color-duo/ResistorColorDuo.cs
+++ resistor-color-duo/ResistorColorDuo.cs
@@ -2,8 +2,27 @@ using System;
 
 public static class ResistorColorDuo
 {
+    private static string[] resistanceValues = {
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    };
+
     public static int Value(string[] colors)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string values = "";
+        foreach (string color in colors) {
+            values += Array.IndexOf(resistanceValues, color);
+        }
+        return Int32.Parse(
+            values.Substring(0, 2)
+        );
     }
 }
