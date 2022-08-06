1ce51700c7963ac9c1f644346cb033b15bba4130
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-08-06 01:54:29 +0100

    Annalyn's Infiltration was too longwinded

--- annalyns-infiltration/AnnalynsInfiltration.cs
+++ annalyns-infiltration/AnnalynsInfiltration.cs
@@ -2,37 +2,19 @@ using System;
 
 static class QuestLogic
 {
-    public static bool CanFastAttack(bool knightIsAwake)
-    {
-        if (!knightIsAwake) return true;
-        else return false;
-    }
+    public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
 
     public static bool CanSpy(
         bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake
-    )
-    {
-        if (knightIsAwake || archerIsAwake || prisonerIsAwake) return true;
-        else return false;
-    }
+    ) => knightIsAwake || archerIsAwake || prisonerIsAwake;
 
     public static bool CanSignalPrisoner(
         bool archerIsAwake, bool prisonerIsAwake
-    )
-    {
-        if (prisonerIsAwake && !archerIsAwake) return true;
-        else return false;
-    }
+    ) => prisonerIsAwake && !archerIsAwake;
 
     public static bool CanFreePrisoner(
         bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake,
         bool petDogIsPresent
-    )
-    {
-        if (
-            (!archerIsAwake && petDogIsPresent)
-            || (!knightIsAwake && !archerIsAwake && prisonerIsAwake)
-        ) return true;
-        else return false;
-    }
+    ) => (!archerIsAwake && petDogIsPresent)
+        || (!knightIsAwake && !archerIsAwake && prisonerIsAwake);
 }
