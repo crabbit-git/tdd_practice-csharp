744784502795bab07d369ee4a8d88bd19e7fe8cd
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2023-01-26 12:09:25 +0000

    Outdated Secret Handshake removed

--- secret-handshake/SecretHandshake.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class SecretHandshake
-{
-    public static string[] Commands(int commandValue)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-}
