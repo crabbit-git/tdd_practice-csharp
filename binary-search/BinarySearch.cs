640d9f02911a7447e0fd07de4fa19f8f532c2e2e
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2023-01-27 11:56:46 +0000

    Redid Binary Search without Array.IndexOf()

--- binary-search/BinarySearch.cs
+++ binary-search/BinarySearch.cs
@@ -2,6 +2,16 @@ using System;
 
 public static class BinarySearch
 {
-    public static int Find(int[] array, int value)
-        => Array.IndexOf(array, value);
+    public static int Find(int[] array, int query)
+    // The thing is, there is already a built in method to do this:
+        // => Array.IndexOf(array, value);
+    // ... But I think writing one manually is meant to be the point of this,
+    // so I reckon I'll break out the C fundamentals:
+
+    {
+        for (int i = 0; i < array.Length; i++) {
+            if (array[i] == query) return i;
+        }
+        return -1;
+    }
 }
