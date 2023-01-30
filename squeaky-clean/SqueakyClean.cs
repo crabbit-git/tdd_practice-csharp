aaeeac8982359cb0053aa3bfafb2afe776173402
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2023-01-30 16:35:00 +0000

    Better use of "case when" syntax

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -5,24 +5,28 @@ public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        StringBuilder formatted = new StringBuilder(identifier)
-                                      .Replace(' ', '_')
-                                      .Replace("\0", "CTRL");
-        
-        for (int i = 0; i < formatted.Length; i++) {
-            if (formatted[i] == '-') {
-            // Note that this is only this simplistic because the final test requires it;
-            // in my opinion, " -a" does not constitute kebab-case due to the space before the '-',
-            // but the test specifies that it should be treated as such and converted to "_A".
-                formatted.Remove(i, 1);
-                formatted[i] = Char.ToUpper(formatted[i]); // Iterator is already on next character due to deletion, hence no i+1
-                i--; // Repositions cursor for further checks, accounting for deletion of 1 char
-            } else if (!Char.IsLetter(formatted[i]) && formatted[i] != '_') {
-                formatted.Remove(i, 1);
-                i--;
-            } else if (Char.IsBetween(formatted[i], 'α', 'ω')) {
-                formatted.Remove(i, 1);
-                i--;
+        StringBuilder formatted = new StringBuilder();
+
+        for (int i = 0; i < identifier.Length; ++i) {
+            switch (identifier[i]) {
+                case ' ':
+                    formatted.Append('_');
+                    break;
+                case '\0':
+                    formatted.Append("CTRL");
+                    break;
+                case '-':
+                // I don't consider " - a" kebab-case, but the final test does
+                    ++i; // Skips to the character after the '-'
+                    formatted.Append(Char.ToUpper(identifier[i]));
+                    break;
+                case char etruscanPurist when !Char.IsLetter(etruscanPurist):
+                    break;
+                case char hellenophobia when Char.IsBetween(hellenophobia, 'α', 'ω'):
+                    break;
+                default:
+                    formatted.Append(identifier[i]);
+                    break;
             }
         }
         return formatted.ToString();
