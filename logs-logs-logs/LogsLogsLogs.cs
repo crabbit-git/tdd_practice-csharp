6eff287459fd6ec2c5d7ee9d92baf8ae67a7c610
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2023-01-30 12:22:31 +0000

    Reformatted with newfangled switch syntax

--- logs-logs-logs/LogsLogsLogs.cs
+++ logs-logs-logs/LogsLogsLogs.cs
@@ -1,6 +1,5 @@
 using System;
 
-// TODO: define the 'LogLevel' enum
 enum LogLevel
 {
     Unknown,
@@ -15,18 +14,15 @@ enum LogLevel
 static class LogLine
 {
     public static LogLevel ParseLogLevel(string logLine)
-    {
-        string lvlCode = logLine.Split('[', ']')[1];
-        switch (lvlCode) {
-            case "TRC": return LogLevel.Trace;
-            case "DBG": return LogLevel.Debug;
-            case "INF": return LogLevel.Info;
-            case "WRN": return LogLevel.Warning;
-            case "ERR": return LogLevel.Error;
-            case "FTL": return LogLevel.Fatal;
-            default: return LogLevel.Unknown;
-        }
-    }
+        => logLine.Split('[', ']')[1] switch {
+            "TRC" => LogLevel.Trace,
+            "DBG" => LogLevel.Debug,
+            "INF" => LogLevel.Info,
+            "WRN" => LogLevel.Warning,
+            "ERR" => LogLevel.Error,
+            "FTL" => LogLevel.Fatal,
+            _     => LogLevel.Unknown
+        };
 
     public static string OutputForShortLog(LogLevel logLevel, string message)
         => $"{(int)logLevel}:{message}";
